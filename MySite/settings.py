ad33f74e578fc9e95af442362cf657a305a644ae
melvinb9@gmail.com <mnguyen00@umail.ucsb.edu>
2015-08-04 02:15:54 -0700

    added env file

--- MySite/settings.py
+++ MySite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default='postgres://etnstmnijnflkv:eYG3gr0LFwywC7eE5axRGkKPL-@ec2-54-83-41-183.compute-1.amazonaws.com:5432/d42af08orihlk1')
+DATABASES['default'] =  dj_database_url.config(default= os.environ['DATABASE_URL'])
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
